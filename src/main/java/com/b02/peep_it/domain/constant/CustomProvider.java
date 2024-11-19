cd7f76784f2063cfda73857b509c9c11af683360
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-11-19 16:00:22 +0900

    feat: add CustomProvider Enum

---
+++ src/main/java/com/b02/peep_it/domain/constant/CustomProvider.java
@@ -0,0 +1,17 @@
+package com.b02.peep_it.domain.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CustomProvider {
+
+    KAKAO("KAKAO", "카카오 인증 회원"),
+    NAVER("NAVER", "네이버 인증 회원"),
+    APPLE("APPLE", "애플 인증 회원")
+    ;
+
+    private final String code;
+    private final String description;
+}
