2b97ef2b9d0590742824fe26f72f1fea82779337
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-11-26 15:13:59 +0900

    chore: add Generation Strategy

---
+++ src/main/java/com/b02/peep_it/domain/PeepReSticker.java
@@ -0,0 +1,13 @@
+package com.b02.peep_it.domain;
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PeepReSticker {
+
+}
