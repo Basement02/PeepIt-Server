efe4402a7e3e7637907571d8e02832bdc4a67974
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-11-26 14:30:04 +0900

    feat: update TermsAgreement entity

--- src/main/java/com/b02/peep_it/domain/TermsAgreement.java
+++ src/main/java/com/b02/peep_it/domain/TermsAgreement.java
@@ -15,28 +15,22 @@ public class TermsAgreement extends BaseTimeEntity {
     @JoinColumn(name = "member_id")
     private Member member; // 회원 고유 ID
 
-    @Column(name = "service_ver", nullable = false)
-    private String serviceVer; // 서비스 이용약관 버전
-
-    @Column(name = "local_ver", nullable = false)
-    private String localVer; // 위치기반 이용약관 버전
-
-    @Column(name = "private_ver", nullable = false)
-    private String privateVer; // 개인정보 이용약관 버전
-
-    @Column(name = "market_ver", nullable = false)
-    private String marketVer; // 마케팅 이용약관 버전
-
     @Column(name = "is_agree", nullable = false)
     private Boolean isAgree; // 마케팅 이용약관 동의 여부
 
     @Builder
-    public TermsAgreement(Member member, String serviceVer, String localVer, String privateVer, String marketVer, Boolean isAgree) {
+    public TermsAgreement(Member member, Boolean isAgree) {
         this.member = member;
-        this.serviceVer = serviceVer;
-        this.localVer = localVer;
-        this.privateVer = privateVer;
-        this.marketVer = marketVer;
         this.isAgree = isAgree;
+    }
+
+    public TermsAgreement setAgree() {
+        this.isAgree = true;
+        return this;
+    }
+
+    public TermsAgreement setDisAgree() {
+        this.isAgree = false;
+        return this;
     }
 }
