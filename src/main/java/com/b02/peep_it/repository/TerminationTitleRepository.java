f81395250151dac39b03f50f4ad5255d46ffe1d3
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-12-03 16:47:48 +0900

    feat: add TerminationTitleRepository

---
+++ src/main/java/com/b02/peep_it/repository/TerminationTitleRepository.java
@@ -0,0 +1,7 @@
+package com.b02.peep_it.repository;
+
+import com.b02.peep_it.domain.TerminationTitle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TerminationTitleRepository  extends JpaRepository<TerminationTitle, Long> {
+}
