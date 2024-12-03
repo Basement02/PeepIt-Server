eceb8733fb0818f90d13565167e3415a4892f526
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-12-03 16:42:31 +0900

    feat: add MemberBlockRepository

---
+++ src/main/java/com/b02/peep_it/repository/MemberBlockRepository.java
@@ -0,0 +1,7 @@
+package com.b02.peep_it.repository;
+
+import com.b02.peep_it.domain.MemberBlock;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberBlockRepository extends JpaRepository<MemberBlock, Long> {
+}
