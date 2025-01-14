ebc051b7d87c01a259889ca29f725c30a4c4dcba
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2025-01-14 15:16:42 +0900

    feat: add tokenfilter at securityconfig

--- src/main/java/com/b02/peep_it/security/SecurityConfig.java
+++ src/main/java/com/b02/peep_it/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.b02.peep_it.security;
 
+import com.b02.peep_it.security.token.JwtUtils;
+import com.b02.peep_it.security.token.TokenFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,13 +9,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-
+    private JwtUtils jwtUtils;
     private String[] permitList = {
             "/**",
     };
@@ -30,8 +34,14 @@ public class SecurityConfig {
                             .requestMatchers(permitList).permitAll()
                             .anyRequest().authenticated();
                 })
+                .addFilterBefore(new TokenFilter(jwtUtils), UsernamePasswordAuthenticationFilter.class)
                 ;
 
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
