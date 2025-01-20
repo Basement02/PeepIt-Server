e6b1e3a578dd4670799a953bdf7c2fe5926706c3
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2025-01-21 04:07:37 +0900

    feat: add CustomError

--- src/main/java/com/b02/peep_it/common/exception/CustomError.java
+++ src/main/java/com/b02/peep_it/common/exception/CustomError.java
@@ -9,7 +9,15 @@ import lombok.Getter;
 public enum CustomError {
 
     // need to custom
-    NEED_TO_CUSTOM("00000", "NEED TO ERROR CUSTOM!");
+    NEED_TO_CUSTOM("00000", "NEED TO ERROR CUSTOM!"),
+
+    /*
+    409
+    -01: 이미 사용 중인 아이디입니다
+     */
+    DUPLICATED_ID("40901", "이미 사용 중인 아이디입니다"),
+
+    ;
 
     private final String code;
     private final String message;
