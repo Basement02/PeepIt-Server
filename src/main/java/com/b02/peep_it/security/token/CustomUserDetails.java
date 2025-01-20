1bc77550489ded2dd968e3eeed395d54de8f4b8c
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2025-01-20 20:48:17 +0900

    feat: create CustomUserDetails

---
+++ src/main/java/com/b02/peep_it/security/token/CustomUserDetails.java
@@ -0,0 +1,53 @@
+package com.b02.peep_it.security.token;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final String username; // 닉네임
+    private final String password = "";
+    private final String uid;  // DB의 사용자 ID
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
