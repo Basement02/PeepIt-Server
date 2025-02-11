2d347cd1e6933eacade0385238c77b1aa4912e24
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2025-02-11 16:42:42 +0900

    build: update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,38 +1,21 @@
-version: 0.0
-os: linux
+#!/usr/bin/env bash
 
-# 배포 파일 설정
-## source: 인스턴스에 복사할 디렉터리 경로
-## destination: 인스턴스에서 파일이 복사되는 위치
-## overwrite: 복사할 위치에 파일이 있는 경우 대체
-files:
-  - source:  /
-    destination: /home/ubuntu/app
-    overwrite: yes
+PROJECT_ROOT="/home/ubuntu/app"
+JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
-# files 섹션에서 복사한 파일에 대한 권한 설정
-## object: 권한이 지정되는 파일 또는 디렉터리
-## pattern (optional): 매칭되는 패턴에만 권한 부여
-## owner (optional): object 의 소유자
-## group (optional): object 의 그룹 이름
-permissions:
-  - object: /
-    pattern: "**"
-    owner: ubuntu
-    group: ubuntu
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-# 배포 이후에 실행할 일련의 라이프사이클
-# 파일을 설치한 후 `AfterInstall` 에서 기존에 실행중이던 애플리케이션을 종료
-# `ApplicationStart` 에서 새로운 애플리케이션을 실행
-## location: hooks 에서 실행할 스크립트 위치
-## timeout (optional): 스크립트 실행에 허용되는 최대 시간이며, 넘으면 배포 실패로 간주됨
-## runas (optional): 스크립트를 실행하는 사용자
-hooks:
-  AfterInstall:
-    - location: scripts/stop.sh
-      timeout: 60
-      runas: ubuntu
-  ApplicationStart:
-    - location: scripts/start.sh
-      timeout: 60
-      runas: ubuntu
+TIME_NOW=$(date +%c)
+
+# build 파일 복사
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+
+# jar 파일 실행
+echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
