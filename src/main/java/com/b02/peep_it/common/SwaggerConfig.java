0dfeddf4ff10b886aff04e6ade6dbba6e3f420f7
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2025-02-28 09:35:32 +0900

    fix: swagger https로 변경

--- src/main/java/com/b02/peep_it/common/SwaggerConfig.java
+++ src/main/java/com/b02/peep_it/common/SwaggerConfig.java
@@ -5,14 +5,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         String jwt = "JWT";
+
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
         Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
                 .name(jwt)
@@ -20,12 +24,16 @@ public class SwaggerConfig {
                 .scheme("bearer")
                 .bearerFormat("JWT")
         );
+
         return new OpenAPI()
-                .components(new Components())
+                .components(components)
                 .info(apiInfo())
                 .addSecurityItem(securityRequirement)
-                .components(components);
+                .servers(List.of(
+                        new Server().url("https://basement02.site").description("Production Server") // HTTPS 강제 적용
+                ));
     }
+
     private Info apiInfo() {
         return new Info()
                 .title("API Test") // API의 제목
