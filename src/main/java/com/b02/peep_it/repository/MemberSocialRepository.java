1926879a3f3238a4591b565c058d8dbdc64352a2
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2025-03-18 11:24:10 +0900

    fix: build error

--- src/main/java/com/b02/peep_it/repository/MemberSocialRepository.java
+++ src/main/java/com/b02/peep_it/repository/MemberSocialRepository.java
@@ -1,6 +1,7 @@
 package com.b02.peep_it.repository;
 
 import com.b02.peep_it.domain.MemberSocial;
+import com.b02.peep_it.domain.constant.CustomProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,5 +9,5 @@ import java.util.Optional;
 
 public interface MemberSocialRepository extends JpaRepository<MemberSocial, Long> {
 //    @Query("SELECT ms FROM MemberSocial ms WHERE ms.provider = ?1 AND ms.providerId = ?2")
-    Optional<MemberSocial> findByProviderAndProviderId(String provider, String providerId);
+    Optional<MemberSocial> findByProviderAndProviderId(CustomProvider provider, String providerId);
 }
