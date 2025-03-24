c180dbd6c08ee3bf0fae7d7df09bbc09914c9e10
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2025-03-24 23:49:57 +0900

    hotfix: update timezone

--- src/main/java/com/b02/peep_it/PeepItApplication.java
+++ src/main/java/com/b02/peep_it/PeepItApplication.java
@@ -2,13 +2,14 @@ package com.b02.peep_it;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class PeepItApplication {
 
 	public static void main(String[] args) {
+		// 애플리케이션 전역 기본 시간대 Asia/Seoul
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(PeepItApplication.class, args);
 	}
-
 }
